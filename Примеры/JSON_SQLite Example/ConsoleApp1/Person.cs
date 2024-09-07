c7004113bea6a6e82f35e4236fb4f90e3c1994eb
Daniil_Klyukin <anatoliikljukin@mail.ru>
2024-09-07 11:05:25 +0400

    remove temp Примеры

--- Примеры/JSON_SQLite Example/ConsoleApp1/Person.cs
+++
@@ -1,2 +0,0 @@
-﻿// Класс человека в сокращенном виде
-public record Person(int Id, string Name, int Age, double Money);
--- Примеры/JSON_SQLite Example/ConsoleApp1/SQLitePersonLoader.cs
+++
@@ -1,9 +0,0 @@
-﻿public class SQLitePersonLoader : IPersonLoader
-{
-    public Person? LoadByName(string dataBasePath, string name)
-    {
-        using ApplicationDBContext db = new(dataBasePath);
-
-        return db.Persons.SingleOrDefault(p => p.Name == name);
-    }
-}
