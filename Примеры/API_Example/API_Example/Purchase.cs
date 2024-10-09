c03260d7cc19f595186f4e75127449977b2f7793
KaBaN4iK357 <anatoliikljukin@mail.ru>
2024-10-09 16:02:00 +0400

    Add API lecture + Example

---
+++ Примеры/API_Example/API_Example/Purchase.cs
@@ -0,0 +1,10 @@
+﻿namespace API_Example
+{
+    public class Purchase
+    {
+        public required int Id { get; set; }
+        public required Product Product { get; set; }
+        public required decimal Price { get; set; }
+        public required int Amount { get; set; }
+    }
+}
